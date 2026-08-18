4214856dcbcd38eb4adf227bb891e86184cd292a
tetsuzoshan <tetechite.chite@gmail.com>
2026-08-18 17:07:05 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/rules/general">
+            to="/docs/01.はじめに/01.serversummary.md">
             サーバーの基本ルールを見る 📖
           </Link>
         </div>
